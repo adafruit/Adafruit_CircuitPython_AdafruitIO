66a5266ee3c999191ac8ecbe91c43eef3de6c3d1
brentru <robots199@me.com>
2019-03-11 12:34:48 -0400

    update adafruit_io -> aio in examples to work with secrets.py

--- examples/adafruit_io_simpletest_esp_at.py
+++ examples/adafruit_io_simpletest_esp_at.py
@@ -59,8 +59,8 @@ wifi = adafruit_espatcontrol_wifimanager.ESPAT_WiFiManager(esp, secrets, status_
 # Set your Adafruit IO Username and Key in secrets.py
 # (visit io.adafruit.com if you need to create an account,
 # or if you need your Adafruit IO key.)
-ADAFRUIT_IO_USER = secrets['adafruit_io_user']
-ADAFRUIT_IO_KEY = secrets['adafruit_io_key']
+ADAFRUIT_IO_USER = secrets['aio_username']
+ADAFRUIT_IO_KEY = secrets['aio_key']
 
 # Create an instance of the Adafruit IO REST client
 io = RESTClient(ADAFRUIT_IO_USER, ADAFRUIT_IO_KEY, wifi)
