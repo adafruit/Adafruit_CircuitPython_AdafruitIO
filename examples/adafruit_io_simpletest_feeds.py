5876119fe4a2a9ebc684dc7dd11bf4739adb6a99
brentru <robots199@me.com>
2019-04-30 12:57:50 -0400

    switch defs, one spi object, pylint

--- examples/adafruit_io_simpletest_feeds.py
+++ examples/adafruit_io_simpletest_feeds.py
@@ -23,16 +23,15 @@ except ImportError:
 
 # ESP32 Setup
 try:
-  esp32_cs = DigitalInOut(board.D9)
-  esp32_ready = DigitalInOut(board.D10)
-  esp32_reset = DigitalInOut(board.D5)
-  spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    esp32_cs = DigitalInOut(board.ESP_CS)
+    esp32_ready = DigitalInOut(board.ESP_BUSY)
+    esp32_reset = DigitalInOut(board.ESP_RESET)
 except AttributeError:
-  esp32_cs = DigitalInOut(board.ESP_CS)
-  esp32_ready = DigitalInOut(board.ESP_BUSY)
-  esp32_reset = DigitalInOut(board.ESP_RESET)
-  spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    esp32_cs = DigitalInOut(board.D9)
+    esp32_ready = DigitalInOut(board.D10)
+    esp32_reset = DigitalInOut(board.D5)
 
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2) # Uncomment for Most Boards
 """Uncomment below for ItsyBitsy M4"""
