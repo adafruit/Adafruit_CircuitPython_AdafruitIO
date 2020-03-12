6195133c6f57f1457b2093cc31ff466d9670484b
brentru <robots199@me.com>
2020-03-12 18:47:50 -0400

    fix location for mmqtt breaking

--- examples/mqtt/adafruit_io_location.py
+++ examples/mqtt/adafruit_io_location.py
@@ -1,6 +1,6 @@
 # Example of tagging data with location values
 # and sending it to an Adafruit IO feed.
-
+import time
 import board
 import busio
 from digitalio import DigitalInOut
@@ -10,7 +10,7 @@ import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 import neopixel
 
 
-from adafruit_minimqtt import MQTT
+import adafruit_minimqtt as MQTT
 from adafruit_io.adafruit_io import IO_MQTT
 
 ### WiFi ###
@@ -73,18 +73,19 @@ def message(client, feed_id, payload):
     # the new value.
     print("Feed {0} received new value: {1}".format(feed_id, payload))
 
-
 # Connect to WiFi
+print("Connecting to WiFi...")
+wifi.connect()
+print("Connected!")
+
+# Initialize MQTT interface with the esp interface
+MQTT.set_socket(socket, esp)
 wifi.connect()
 
 # Initialize a new MQTT Client object
-mqtt_client = MQTT(
-    socket=socket,
-    broker="io.adafruit.com",
-    username=secrets["aio_user"],
-    password=secrets["aio_key"],
-    network_manager=wifi
-)
+mqtt_client = MQTT.MQTT(broker="https://io.adafruit.com",
+                        username=secrets["aio_user"],
+                        password=secrets["aio_key"])
 
 # Initialize an Adafruit IO MQTT Client
 io = IO_MQTT(mqtt_client)
@@ -110,5 +111,15 @@ io.publish("location", data_value, metadata)
 print("Data sent!")
 
 
-# Listen forever...
-io.loop_blocking()
+# Start a blocking message loop...
+# NOTE: NO code below this loop will execute
+# NOTE: Network reconnection is handled within this loop
+while True:
+    try:
+        io.loop()
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        io.reconnect()
+        continue
+    time.sleep(1)
