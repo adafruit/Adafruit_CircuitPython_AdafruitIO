c7e8dff3da8ea19ed406df2020f2256461c843e0
brentru <robots199@me.com>
2020-03-12 19:02:38 -0400

    fix imports

--- examples/mqtt/adafruit_io_simpletest_eth.py
+++ examples/mqtt/adafruit_io_simpletest_eth.py
@@ -7,12 +7,12 @@
 import time
 from random import randint
 
-
 import board
 import busio
+from digitalio import DigitalInOut
+
 from adafruit_wiznet5k.adafruit_wiznet5k import WIZNET5K
 import adafruit_wiznet5k.adafruit_wiznet5k_socket as socket
-from digitalio import DigitalInOut
 from adafruit_io.adafruit_io import IO_MQTT
 import adafruit_minimqtt as MQTT
 
