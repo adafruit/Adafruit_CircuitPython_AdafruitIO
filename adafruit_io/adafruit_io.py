f35e2d0fe2cfbd091fee2e2470399d186f1a99e9
askpatricw <4002194+askpatrickw@users.noreply.github.com>
2021-01-26 16:19:44 -0800

    fix URL typos

--- adafruit_io/adafruit_io.py
+++ adafruit_io/adafruit_io.py
@@ -28,7 +28,7 @@ from adafruit_io.adafruit_io_errors import (
 )
 
 __version__ = "0.0.0-auto.0"
-__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_Adafruit_IO.git"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_AdafruitIO.git"
 
 CLIENT_HEADERS = {"User-Agent": "AIO-CircuitPython/{0}".format(__version__)}
 
